d339bc2084baef395d09bd9e6722eb45ca3c1477
Qobiljon <qobiljon.toshnazarov@gmail.com>
2020-04-12 10:59:23 +0900

    some changes

--- www/main.cpp
+++ www/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
 
+void _array() {
+    cout << "Hello World" << endl;
+}
+
+void _pointer() {
+    
+}
+
 int main() {
-    cout << "Hello world!" << endl;
-    cout << "Bye bye world!" << endl;
+    _array();
+    _pointer();
+    
     return 0;
 }
