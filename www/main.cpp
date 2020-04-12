0a49ab688f1cf2f08568bfb17eb3681770f52f65
Qobiljon <qobiljon.toshnazarov@gmail.com>
2020-04-12 11:07:16 +0900

    sodmasd

--- www/main.cpp
+++ www/main.cpp
@@ -3,14 +3,17 @@
 using namespace std;
 
 void _array() {
-    int arr[10];
     /*
-    
     1. if(condition){}
     1. while(condition){}
     2. for(init; condition; increment) {}
-     
      */
+    
+    int arr[10];
+    for(int n = 0; n < 10; n++)
+        arr[n] = n + 10;
+    
+    cout << arr[5] << endl;
 }
 
 void _pointer() {
