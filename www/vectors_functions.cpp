7def04e66393df9dbd473c7098ce9cb329686f55
Qobiljon <qobiljon.toshnazarov@gmail.com>
2020-04-12 12:37:44 +0900

    isdias

--- www/vectors_functions.cpp
+++ www/vectors_functions.cpp
@@ -1,27 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
+void func(string a) {
+    cout << "func() is running: " << a << endl;
+}
 
-int main_1() {
-    // ((void(*)(string))(print))("hi");
+int main() {
+//    int arr0[10];
+//    for(int n = 0; n < 10; n++)
+//        arr0[n] = n;
+//
+//
+//    int N = 5;
+//    vector<int>arr;
+//    for(int n = 0; n < 10; n++)
+//        arr.push_back(arr0[n]);
+//
+//    for(int n = 0; n < N; n++)
+//        arr.push_back(n);
+//    arr.erase(arr.begin() + N/2);
+//
+//    for(int n = 0; n < arr.size(); n++)
+//        cout << arr[n] << endl;
     
-    int arr0[10];
-    for(int n = 0; n < 10; n++)
-        arr0[n] = n;
+    int (*myPrint)(const char * __restrict, ...) = printf;
+    myPrint("printf() is running\n");
     
-    
-    int N = 5;
-    vector<int>arr;
-    for(int n = 0; n < 10; n++)
-        arr.push_back(arr0[n]);
-    
-    for(int n = 0; n < N; n++)
-        arr.push_back(n);
-    arr.erase(arr.begin() + N/2);
-    
-    for(int n = 0; n < arr.size(); n++)
-        cout << arr[n] << endl;
+    void (*myFunc)(string) = func;
+    myFunc("hola");
     
     return 0;
 }
